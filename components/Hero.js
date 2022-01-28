09db72973516ccb9c88e053109eefc3b7ea6697f
Fabian <acostahf4@gmail.com>
2022-01-28 13:37:12 -0600

    gsap intergation

--- components/Hero.js
+++ components/Hero.js
@@ -6,20 +6,26 @@ import {
   SimpleGrid,
   Text,
 } from "@chakra-ui/layout";
-import React from "react";
+import { gsap } from "gsap";
+import React, { useEffect } from "react";
 
 export const Hero = () => {
+  useEffect(() => {
+    var tl = gsap.timeline();
+    tl.from(".element", { duration: 2, y: 100, opacity: 0, stagger: 0.5 });
+  });
+
   return (
     <Box>
       <SimpleGrid minH="100vh" columns={2}>
         <Flex justify="center" align="center" flexDirection="column" p="10">
           <Box>
-            <Text fontWeight="semibold" fontSize="xl">
+            <Text className="element" fontWeight="semibold" fontSize="xl">
               {" "}
               Cillum non consequat tempor ad tempor nulla laboris irure irure
               officia.
             </Text>
-            <Heading as="h1" size="3xl">
+            <Heading className="element" as="h1" size="3xl">
               {" "}
               Nulla ullamco in excepteur nostrud proident.
             </Heading>
--- components/ServicesHero.js
+++ components/ServicesHero.js
@@ -6,17 +6,22 @@ import {
   SimpleGrid,
   Text,
 } from "@chakra-ui/layout";
-import React from "react";
+import gsap from "gsap";
+import React, { useEffect } from "react";
 
 export const ServicesHero = () => {
+  useEffect(() => {
+    var tl = gsap.timeline();
+    tl.from(".element", { duration: 2, y: 100, opacity: 0, stagger: 0.5 });
+  });
   return (
     <Box minH="100vh">
       <Flex paddingTop="10" justify="center">
-        <Heading fontSize="5xl" fontWeight="bold" as="h1">
+        <Heading className="element" fontSize="5xl" fontWeight="bold" as="h1">
           Services
         </Heading>
       </Flex>
-      <SimpleGrid columns={2}>
+      <SimpleGrid className="element" columns={2}>
         <Flex justify="center" align="center" flexDirection="column" p="10">
           <Box>
             <Text fontWeight="semibold" fontSize="4xl" paddingBottom="5">
