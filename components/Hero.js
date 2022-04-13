30dadf3b2991ef1dd2e3853199cb125c4582294d
Fabian <acostahf4@gmail.com>
2022-04-13 10:47:34 -0500

    form

--- components/Hero.js
+++ components/Hero.js
@@ -1,76 +1,153 @@
 import Image from "next/image";
 import {
-  Box,
-  Flex,
-  GridItem,
-  Heading,
-  SimpleGrid,
-  Text,
-  Stack,
+	Box,
+	Flex,
+	GridItem,
+	Heading,
+	SimpleGrid,
+	Text,
+	Stack,
 } from "@chakra-ui/layout";
-import { Input, InputGroup, InputRightAddon } from "@chakra-ui/react";
+import { Alert } from "@chakra-ui/react";
+import { Button, Input, InputGroup, InputRightAddon } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 export const Hero = () => {
-  return (
-    <Box>
-      <SimpleGrid
-        minH="80vh"
-        columns={{ base: 1, sm: 1, md: 2 }}
-        bg="brand.200"
-        bgImage={"url('/header.png')"}
-        bgPosition="center"
-        bgSize="cover"
-        bgRepeat="no-repeat"
-        color="brand.300"
-      >
-        <Flex justify="center" align="center" flexDirection="column" p="10">
-          <Stack spacing={10}>
-            <Box>
-              <Heading
-                className="element"
-                // fontWeight="semibold"
-                fontSize="xl"
-                fontFamily={"body"}
-              >
-                No Matter Who You Are.
-              </Heading>
-              <Heading className="element" as="h1" size="4xl">
-                Safety <br /> Made Easy.
-              </Heading>
-            </Box>
-            <InputGroup className="element" maxW={"xl"}>
-              <Input
-                borderLeftRadius="3xl"
-                borderColor="brand.200"
-                bg="brand.300"
-                placeholder="EMAIL ADDRESS"
-                fontFamily={"heading"}
-                fontSize={"x-small"}
-              />
-              <InputRightAddon
-                borderRightRadius="3xl"
-                borderColor="brand.200"
-                fontFamily={"heading"}
-                fontSize={"sm"}
-                bg="brand.400"
-              >
-                GET IN TOUCH
-              </InputRightAddon>
-            </InputGroup>
-          </Stack>
-        </Flex>
-        <GridItem
-          h={{ base: "0", sm: "0", md: "auto" }}
-          bg="gray.300"
-          position="relative"
-          bgImage="url('/header.jpg') "
-          bgPosition="center"
-          bgSize="cover"
-          bgRepeat="no-repeat"
-        >
-          {/* <Image src="/header.jpg" alt="header imager" layout="fill" priority /> */}
-        </GridItem>
-      </SimpleGrid>
-    </Box>
-  );
+	const [email, setEmail] = useState("");
+	const [errors, setErrors] = useState({});
+	const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+	const [showFailureMessage, setShowFailureMessage] = useState(false);
+	const [buttonText, setButtonText] = useState("Send");
+
+	const handleValidation = () => {
+		let tempErrors = {};
+		let isValid = true;
+
+		if (email.length <= 0) {
+			tempErrors["email"] = true;
+			isValid = false;
+		}
+
+		setErrors({ ...tempErrors });
+		console.log("errors", errors);
+		return isValid;
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		let isValidForm = handleValidation();
+
+		if (isValidForm) {
+			setButtonText("Sending");
+			const res = await fetch("/api/email", {
+				body: JSON.stringify({
+					email: email,
+				}),
+				headers: {
+					"Content-Type": "application/json",
+				},
+				method: "POST",
+			});
+
+			const { error } = await res.json();
+			if (error) {
+				console.log(error);
+				setShowSuccessMessage(false);
+				setShowFailureMessage(true);
+				setButtonText("Send");
+				return;
+			}
+			setShowSuccessMessage(true);
+			setShowFailureMessage(false);
+			setButtonText("Send");
+		}
+		console.log(email);
+	};
+
+	return (
+		<Box>
+			<SimpleGrid
+				minH="80vh"
+				columns={{ base: 1, sm: 1, md: 2 }}
+				bg="brand.200"
+				bgImage={"url('/header.png')"}
+				bgPosition="center"
+				bgSize="cover"
+				bgRepeat="no-repeat"
+				color="brand.300"
+			>
+				<Flex justify="center" align="center" flexDirection="column" p="10">
+					<Stack spacing={10}>
+						<Box>
+							<Heading
+								className="element"
+								// fontWeight="semibold"
+								fontSize="xl"
+								fontFamily={"body"}
+							>
+								No Matter Who You Are.
+							</Heading>
+							<Heading className="element" as="h1" size="4xl">
+								Safety <br /> Made Easy.
+							</Heading>
+						</Box>
+						<form onSubmit={handleSubmit}>
+							<div className="text-left pl-3">
+								{showSuccessMessage && (
+									<Alert status="success" color={"black"}>
+										Thankyou! Your Message has been delivered.{" "}
+									</Alert>
+								)}
+								{showFailureMessage && (
+									<Alert status="error" color={"black"}>
+										Oops! Something went wrong, please try again.{" "}
+									</Alert>
+								)}
+							</div>
+							<InputGroup className="element" maxW={"xl"}>
+								<Input
+									borderLeftRadius="3xl"
+									borderColor="brand.200"
+									bg="brand.300"
+									color={"brand.200"}
+									placeholder="EMAIL ADDRESS"
+									fontFamily={"heading"}
+									fontSize={"x-small"}
+									type="email"
+									value={email}
+									onChange={(e) => {
+										// setSubject(e.target.value);
+										setEmail(e.target.value);
+									}}
+								/>
+								<InputRightAddon
+									borderRightRadius="3xl"
+									borderColor="brand.200"
+									fontFamily={"heading"}
+									fontSize={"sm"}
+									bg="brand.400"
+								>
+									<button type="submit">GET IN TOUCH</button>
+								</InputRightAddon>
+							</InputGroup>
+						</form>
+					</Stack>
+				</Flex>
+				<GridItem
+					h={{ base: "0", sm: "0", md: "auto" }}
+					bg="gray.300"
+					position="relative"
+					bgImage="url('/header.jpg') "
+					bgPosition="center"
+					bgSize="cover"
+					bgRepeat="no-repeat"
+				>
+					{/* <Image src="/header.jpg" alt="header imager" layout="fill" priority /> */}
+				</GridItem>
+			</SimpleGrid>
+		</Box>
+	);
 };
+
+export default Hero;
--- section/homepage/join.js
+++ section/homepage/join.js
@@ -1,116 +1,234 @@
-import React from "react";
-import Image from "next/image";
 import {
-  Box,
-  Flex,
-  GridItem,
-  Heading,
-  SimpleGrid,
-  Text,
+	Box,
+	Flex,
+	GridItem,
+	Heading,
+	SimpleGrid,
+	Text,
 } from "@chakra-ui/layout";
 import {
-  Input,
-  InputGroup,
-  InputRightAddon,
-  Textarea,
-  Button,
-  ButtonGroup,
+	Input,
+	InputGroup,
+	InputRightAddon,
+	Textarea,
+	Button,
+	ButtonGroup,
+	Alert,
 } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const Join = () => {
-  return (
-    <Box display={{ base: "none", sm: "block" }}>
-      <SimpleGrid
-        minH={"40vh"}
-        columns={{ base: 1, sm: 1, md: 3 }}
-        bg="brand.200"
-        color="brand.200"
-        bgImage={"url('/mid-pattern.svg')"}
-        bgPosition="center"
-        bgSize="cover"
-        bgRepeat="no-repeat"
-      >
-        <GridItem
-          bg="gray.300"
-          colSpan={2}
-          p="10"
-          backgroundImage="url('/staircase-to-worker.jpg')"
-          backgroundPosition="left"
-          backgroundRepeat="no-repeat"
-          backgroundSize="cover"
-        >
-          {/* <Image src="/staircase-to-worker.jpg" alt="staircase" layout="fill" /> */}
-          <Flex height={"full"}>
-            <Flex
-              maxW="sm"
-              zIndex="2"
-              justifyContent="center"
-              flexDirection="column"
-            >
-              <Heading className="element" as="h1" size="lg">
-                JOIN OUR TEAM
-              </Heading>
-              <Text className="element" fontSize="sm" maxW={"xs"}>
-                Looking at safety roles? Join our roster and be paired with a
-                jobsite across the country with competitive pay. The TXO way!
-              </Text>
-            </Flex>
-          </Flex>
-        </GridItem>
-        <GridItem>
-          <Flex
-            justify="center"
-            align="center"
-            flexDirection="column"
-            p="10"
-            h={"full"}
+	// States for contact form fields
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
 
-            // height="lg"
-          >
-            <InputGroup className="element" flexDirection="column" gap="4">
-              <Input
-                borderRadius="3xl"
-                borderColor="brand.200"
-                fontSize={"md"}
-                bg="brand.300"
-                placeholder="First and Last Name"
-              />
-              <Input
-                borderRadius="3xl"
-                borderColor="brand.200"
-                fontSize={"md"}
-                bg="brand.300"
-                placeholder="Email Address"
-              />
-              <Textarea
-                borderRadius="md"
-                borderColor="brand.200"
-                fontSize={"md"}
-                h={"32"}
-                bg="brand.300"
-                placeholder="Brief Message"
-                size="lg"
-              />
-              <ButtonGroup>
-                <Button
-                  rounded="full"
-                  px="6"
-                  bgColor="brand.400"
-                  _hover={{ bg: "brand.100" }}
-                  color="white"
-                  variant="solid"
-                  fontFamily={"heading"}
-                  fontSize={"x-small"}
-                >
-                  GET IN TOUCH
-                </Button>
-              </ButtonGroup>
-            </InputGroup>
-          </Flex>
-        </GridItem>
-      </SimpleGrid>
-    </Box>
-  );
+	//   Form validation stateds
+	const [errors, setErrors] = useState({});
+
+	//   Setting button text on form submission
+	const [buttonText, setButtonText] = useState("Send");
+
+	// Setting success or failure messages states
+	const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+	const [showFailureMessage, setShowFailureMessage] = useState(false);
+
+	// Validation check method
+	const handleValidation = () => {
+		let tempErrors = {};
+		let isValid = true;
+
+		if (name.length <= 0) {
+			tempErrors["name"] = true;
+			isValid = false;
+		}
+
+		if (email.length <= 0) {
+			tempErrors["email"] = true;
+			isValid = false;
+		}
+		if (message.length <= 0) {
+			tempErrors["message"] = true;
+			isValid = false;
+		}
+
+		setErrors({ ...tempErrors });
+		console.log("errors", errors);
+		return isValid;
+	};
+
+	//   Handling form submit
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		let isValidForm = handleValidation();
+
+		if (isValidForm) {
+			setButtonText("Sending");
+			const res = await fetch("/api/footerForm", {
+				body: JSON.stringify({
+					email: email,
+					name: name,
+					message: message,
+				}),
+				headers: {
+					"Content-Type": "application/json",
+				},
+				method: "POST",
+			});
+
+			const { error } = await res.json();
+			if (error) {
+				console.log(error);
+				setShowSuccessMessage(false);
+				setShowFailureMessage(true);
+				setButtonText("Send");
+				return;
+			}
+			setShowSuccessMessage(true);
+			setShowFailureMessage(false);
+			setButtonText("Send");
+		}
+		console.log(name, email, message);
+	};
+
+	return (
+		<Box display={{ base: "none", sm: "block" }}>
+			<SimpleGrid
+				minH={"40vh"}
+				columns={{ base: 1, sm: 1, md: 3 }}
+				bg="brand.200"
+				color="brand.200"
+				bgImage={"url('/mid-pattern.svg')"}
+				bgPosition="center"
+				bgSize="cover"
+				bgRepeat="no-repeat"
+			>
+				<GridItem
+					bg="gray.300"
+					colSpan={2}
+					p="10"
+					backgroundImage="url('/staircase-to-worker.jpg')"
+					backgroundPosition="left"
+					backgroundRepeat="no-repeat"
+					backgroundSize="cover"
+				>
+					{/* <Image src="/staircase-to-worker.jpg" alt="staircase" layout="fill" /> */}
+					<Flex height={"full"}>
+						<Flex
+							maxW="sm"
+							zIndex="2"
+							justifyContent="center"
+							flexDirection="column"
+						>
+							<Heading className="element" as="h1" size="lg">
+								JOIN OUR TEAM
+							</Heading>
+							<Text className="element" fontSize="sm" maxW={"xs"}>
+								Looking at safety roles? Join our roster and be paired with a
+								jobsite across the country with competitive pay. The TXO way!
+							</Text>
+						</Flex>
+					</Flex>
+				</GridItem>
+
+				<GridItem>
+					<Flex
+						justify="center"
+						// align="center"
+						flexDirection="column"
+						p="10"
+						w={"full"}
+						h={"full"}
+
+						// height="lg"
+					>
+						<form onSubmit={handleSubmit}>
+							<div className="text-left pl-3">
+								{showSuccessMessage && (
+									<Alert status="success" color={"black"}>
+										Thankyou! Your Message has been delivered.{" "}
+									</Alert>
+								)}
+								{showFailureMessage && (
+									<Alert status="error" color={"black"}>
+										Oops! Something went wrong, please try again.{" "}
+									</Alert>
+								)}
+							</div>
+							<InputGroup
+								className="element"
+								flexDirection="column"
+								gap="4"
+								w={"full"}
+								h={"full"}
+							>
+								<Input
+									borderRadius="3xl"
+									borderColor="brand.200"
+									fontSize={"md"}
+									bg="brand.300"
+									placeholder="First and Last Name"
+									type="text"
+									value={name}
+									name="name"
+									onChange={(e) => {
+										// setSubject(e.target.value);
+										setName(e.target.value);
+									}}
+								/>
+								{errors?.name && <Text> Name cannot be empty.</Text>}
+								<Input
+									borderRadius="3xl"
+									borderColor="brand.200"
+									fontSize={"md"}
+									bg="brand.300"
+									placeholder="Email Address"
+									type="email"
+									value={email}
+									onChange={(e) => {
+										setEmail(e.target.value);
+									}}
+								/>
+								{errors?.email && <Text> Email cannot be empty.</Text>}
+								<Textarea
+									borderRadius="md"
+									borderColor="brand.200"
+									fontSize={"md"}
+									h={"32"}
+									bg="brand.300"
+									placeholder="Brief Message"
+									size="lg"
+									type="text"
+									value={message}
+									onChange={(e) => {
+										setMessage(e.target.value);
+									}}
+								/>
+								{errors?.message && <Text> Message cannot be empty.</Text>}
+								<ButtonGroup>
+									<Button
+										type="submit"
+										rounded="full"
+										px="6"
+										bgColor="brand.400"
+										_hover={{ bg: "brand.100" }}
+										color="white"
+										variant="solid"
+										fontFamily={"heading"}
+										fontSize={"x-small"}
+									>
+										GET IN TOUCH
+									</Button>
+								</ButtonGroup>
+							</InputGroup>
+						</form>
+					</Flex>
+				</GridItem>
+			</SimpleGrid>
+		</Box>
+	);
 };
 
 export default Join;
